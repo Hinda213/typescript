c081ee6659a60dab131566c00475fcf34dc703de
Hinda <profhindo@gmail.com>
2025-10-17 11:20:17 +0300

    add objects, type annotations, functions in typescriptfile

---
+++ basic_types/types.ts
@@ -0,0 +1,13 @@
+let namE: string = "Ali";
+let age: number = 25;
+let isActive: boolean = true;
+
+let hobbies: string[] =["reading", "gaming"];
+let person: [string, number] = ["Ali", 25];
+
+let anything: any = "Can be anything";
+
+
+function greet1(): void{
+    console.log("Hi");
+}
